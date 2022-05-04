9d9f51290d10b75e76f825cb4598fd50e59d1915
Daru Broodryk <dbroodryk@outlook.com>
2022-05-04 10:54:36 +0200

    Comment cleaning

--- Data Migration/data_migration_cleaning.sql
+++ Data Migration/data_migration_cleaning.sql
@@ -1,8 +1,9 @@
 begin tran
 --commit
 --rollback
-
-----------------------------------Sections that does not have Tax--------------------------------------
+-----------------------------------------------------------------------------------------------
+                               --------Sections that does not have Tax--------
+-----------------------------------------------------------------------------------------------
 
 insert into ProductSectionTax
 (FKProductSectionID,FKTaxID,FKPrimaryTaxAdministratorID,FKSecondaryTaxAdministratorID,CreatedBy,ModifiedDate,Active,Comment)
@@ -22,9 +23,12 @@ from
 	where PKProductSectionID not in (select FKProductSectionID from ProductSectionTax)
 	) as my_derived_table
 
-/*---------------------------------ClientPolicy----------------------------------------*/
+-----------------------------------------------------------------------------------------------
+                               --------ClientPolicy--------
+-----------------------------------------------------------------------------------------------
 
-------------------Update missing products on ClientPolicy---------------------------------------
+
+--Update missing products on ClientPolicy
 
 update ClientPolicy
 set 
@@ -37,7 +41,10 @@ where
 	ClientPolicy.Product is null
 
 
---------------------------------------Product fix---------------------------------------------------------
+-----------------------------------------------------------------------------------------------
+                               --------Product fix--------
+-----------------------------------------------------------------------------------------------
+
 
 print 'Product1' 
 update ClientPolicy
@@ -47,7 +54,7 @@ where Product = 'Product1' and CompanyName is not null
 update ClientPolicy
 set Product = 'Product1 Domestic'
 where Product = 'Product1' and CompanyName is null
-------
+
 print 'Product2 Financial Services'
 update ClientPolicy
 set Product = 'Product2  Commercial'
@@ -56,12 +63,12 @@ where Product = 'Product2 Financial Services' and CompanyName is not null
 update ClientPolicy
 set Product = 'Product2 Domestic'
 where Product = 'Product2 Financial Services' and CompanyName is null
--------
+
 print 'Product3'
 update ClientPolicy
 set Product = 'Product3 Commercial'
 where Product = 'Product3' 
--------
+
 print 'Product4 Risk Solutions'
 update ClientPolicy
 set Product = 'Product4 Risk Commercial'
@@ -71,21 +78,19 @@ update ClientPolicy
 set Product = 'Product4 Risk Domestic'
 where Product = 'Product4 Risk Solutions' and CompanyName is null
 
+--There are companies with a domestic product?
 
-
-------------------------------There are companies with a domestic product?-------------------------
 print 'Product5'
 update ClientPolicy
 set Product = 'Product5 Domestic Product'
 where Product = 'Product5' 
 
-----------There are companies with a domestic product, the companies does not have a name----------
-print 'Product6'--1 result extra
+print 'Product6'
 update ClientPolicy
 set Product = 'Product6 Domestic'
 where Product = 'Product6' 
 
--------Product7 Domestic spelled incorrectly
+--Product7 Domestic spelled incorrectly
 print 'Product7 Insurance Brokers'
 update ClientPolicy
 set Product = 'Product7 Commercil'
@@ -94,7 +99,7 @@ where Product = 'Product7 Insurance Brokers' and CompanyName is not null
 update ClientPolicy
 set Product = 'Product7 Domestic'
 where Product = 'Product7 Insurance Brokers' and CompanyName is null
----------
+
 print 'Product8'
 update ClientPolicy
 set Product = 'Product8 Wealth and Risk Commercial'
@@ -104,7 +109,10 @@ update ClientPolicy
 set Product = 'Product8 Wealth and Risk Domestic'
 where Product = 'Product8' and CompanyName is null
 
----------------------------------Deal with --------------------------------------
+-----------------------------------------------------------------------------------------------
+                               --------Stuff to deal with--------
+-----------------------------------------------------------------------------------------------
+
 select 
 	FKPolicyID,
 	min(isnull(CoverItems.InceptionDate,AddedDate)) as InceptDate
@@ -127,23 +135,17 @@ from
 
 drop table #UpdateTable
 
----------------------------------Not sure what to do with Agri--------------------------------------
-
+--Confirm what to do with Agri?
 
 print 'Product9'
 update ClientPolicy 
 set Product = 'Product9 Makelaars Commercial Agri'
 where Product = 'Product9' and CompanyName is not null
 
---update ClientPolicy
---set Product = 'Product9 Makelaars Commercial'
---where Product = 'Product9' and CompanyName is not null
-
 update ClientPolicy
 set Product = 'Product9 Makelaars Domestic'
 where Product = 'Product9' and CompanyName is null
 
------------ 
 print 'Product10'
 update ClientPolicy
 set Product = 'Product10 Commercial'
@@ -152,7 +154,7 @@ where Product = 'Product10' and CompanyName is not null
 update ClientPolicy
 set Product = 'Product10 Domestic'
 where Product = 'Product10' and CompanyName is null
------------
+
 print 'Product11'
 update ClientPolicy
 set Product = 'Product11 Makelaars Commercial'
@@ -162,12 +164,12 @@ update ClientPolicy
 set Product = 'Product11 Makelaars Domestic'
 where Product = 'Product11' and CompanyName is null
 
------------has one company linked to a domestic product?
-print 'Product12' --1 result out has 9 instead of 10
+--has one company linked to a domestic product?
+print 'Product12' 
 update ClientPolicy
 set Product = 'Product12 Domestic'
 where Product = 'Product12' 
-------------
+
 print 'Product13 Insurance Brokers' 
 update ClientPolicy
 set Product = 'Product13 Insurance Brokers Commercial'
@@ -178,14 +180,12 @@ set Product = 'Product13 Insurance Brokers Domestic'
 where Product = 'Product13 Insurance Brokers' and CompanyName is null
 
 
---------------------------------SmallDate coversion error -----------------------------------
+--SmallDate coversion error
 update ClientPolicy
 set inceptionDate = '1900-01-01'
 where inceptionDate < '1900-01-01'
 
-
--------------------------------Cellphone number correction-----------------------------------
---------set 0 to null 
+--Cellphone number correction
 update ClientPolicy
 set Insured_CellphoneNumber = null 
 where Insured_CellphoneNumber = '0' 
@@ -210,7 +210,7 @@ update ClientPolicy
 set FaxNumber = null 
 where FaxNumber = '0' 
 
---------add +27 infront
+--add +27 infront
 update ClientPolicy
 set Insured_CellphoneNumber = concat('(+27)', Insured_CellphoneNumber)
 where charindex('0', Insured_CellphoneNumber) <> 1 
@@ -235,7 +235,7 @@ update 	ClientPolicy
 set FaxNumber = concat('(+27)', FaxNumber)
 where charindex('0', FaxNumber) <> 1 
 
----------replace already existing 0 with +27
+--replace already existing 0 with +27
 update ClientPolicy
 set Insured_CellphoneNumber = replace(Insured_CellphoneNumber,left(Insured_CellphoneNumber, 1),'(+27)')
 where charindex('0', Insured_CellphoneNumber) = 1
@@ -260,7 +260,7 @@ update 	ClientPolicy
 set FaxNumber = replace(FaxNumber,left(FaxNumber, 1),'(+27)')
 where charindex('0', FaxNumber) = 1
 
-------------remove spacing
+--remove spacing
 update ClientPolicy
 set Insured_CellphoneNumber = replace(Insured_CellphoneNumber,' ','')
 
@@ -279,19 +279,21 @@ set WorkPhoneNumber = replace(WorkPhoneNumber,' ','')
 update 	ClientPolicy
 set FaxNumber = replace(FaxNumber,' ','')
 
-/*---------------------------------RiskItems----------------------------------------*/
---------------------------------SmallDate coversion error -----------------------------------
+--------------------------------------------------------------------------------------------
+                           -----------RiskItems-------------
+---------------------------------------------------------------------------------------------
 
+--SmallDate coversion error
 update RiskItems
 set EffectiveDate = '1900-01-01'
 where EffectiveDate < '1900-01-01'
 
----------------------------------CHK_CoverItems_Dates2-----------------------------------------
+--CHK_CoverItems_Dates2
 update RiskItems
 set EndDate = null
 where EndDate = '1900-01-01 00:00:00.000'
 
---------------------------"" around answer columns in RiskItems--------------------------
+--"" around answer columns in RiskItems
 
 update RiskItems
 set Answer001 = replace(Answer001,'"','')
@@ -315,8 +317,7 @@ update RiskItems
 set Answer007 = replace(Answer007,'"','')
 
 
---------------------------------- YOM data issues--------------------------------------------
-
+--YOM data issues
 update RiskItems
 set Answer005 = '2018'
 where RIskID In (N'3CFFCE28-D692-4A2B-B479-4EB73E30E76F', N'BCF0A82D-A2D5-4113-9C12-4EEC3E14E9C3')
@@ -334,13 +335,12 @@ set Answer005 = '2010'
 where RIskID In (N'2CBF045C-8FBF-4BB9-A90F-CEF127588C53', N'8B65E11B-8958-4154-8D9F-1B6E54388B97', N'EF9DDB1D-8EDE-4610-BC8F-E3EE0A3BB506', N'4CD4222B-7D0A-4BC0-BB67-62F8EA556BAA', N'C9C3EECD-5424-438E-BB5B-7996F04F996F', N'C4037614-CD9C-4D20-8DC2-BEA7BAB863CC')
 
 
----------------------------------CHK_tblVehicle_YOM------------------------------------------
-
+--CHK_tblVehicle_YOM
 update RiskItems
 set Answer005 = null
 where  Answer005 in (N'0', N'0000', N'TBA', N'VOLG')
 
-------------------------------UMA Product Spelled incorrectly--------------------------------
+--UMA Product Spelled incorrectly
 update UMAProductSection
 set Name = 'Excess Waiver Basic & Windscreen'
 where Name = 'Excess Waiver Basic  & Windscreen'
@@ -358,13 +358,15 @@ set Covername  = 'Excess Waiver Basic'
 where Covername = 'Excess waiver'
 
 
----------------------------- Risk Items EndDate correction--------------------------------
+--Risk Items EndDate correction
 
 update RiskItems
 set EndDate = null
 where  EndDate = '1900-01-01 00:00:00.000'
 
-------------------------------------Update Peril-----------------------------------------------
+-----------------------------------------------------------------------------------------------
+                               --------Update Peril--------
+-----------------------------------------------------------------------------------------------
 update Claim
 set [Claim Peril] = 'General'
 
@@ -392,8 +394,9 @@ getdate(),
 1,
 null
 )
-
-------------------------------------Update Causes-------------------------------------------
+-----------------------------------------------------------------------------------------------
+                               --------Update Causes--------
+-----------------------------------------------------------------------------------------------
 
 update Claim
 set [Claim Cause] = 'Power surge'
@@ -449,9 +452,9 @@ where ClaimCause.FKClaimPerilID is null
 order by ClaimPeril.PKClaimPerilID
 
 
-
------------------------------Section CoverNames corrected---------------------------------------
-*/
+-----------------------------------------------------------------------------------------------
+                               --------Section CoverNames corrected--------
+-----------------------------------------------------------------------------------------------
 
 update RiskItems
 set CoverName = 'OneVAP Car Hire 30 Days'
@@ -477,67 +480,54 @@ update RiskItems
 set CoverName = 'Car Hire'
 where CoverName  = 'Car Hire - 30 Days' and Product = 'Product2 Financial Services'
 
-
 update RiskItems
 set CoverName = 'Product11 Excess Waiver'
 where Product = 'Product11'	and CoverName = 'Excess Waiver Basic & Windscreen'
 
--------
 update RiskItems
 set CoverName = 'Excess waiver- R3500'
 where Product = 'Product9' and CoverName = 'Excess Waiver Basic & Windscreen'
 
--------
 update RiskItems
 set CoverName = 'Excess Waiver - R5000'
 where Product = 'Product10' and CoverName = 'Excess Waiver Basic'
 
--------
 update RiskItems
 set CoverName = 'Excess Waiver - R5000'
 where Product = 'Product5' and CoverName = 'Excess Waiver Basic & Windscreen'
 
--------
 update RiskItems
 set CoverName = 'Excess Waiver'
 where Product = 'Product2 Financial Services'	and CoverName = 'Excess Waiver Basic'
 
--------
 update RiskItems
 set CoverName = 'Excess Waiver  R5000'
 where Product = 'Product4 Risk Solutions' and CoverName = 'Excess Waiver Basic'
 
--------
 update RiskItems
 set CoverName = 'Car hire'
 where Product = 'Product7 Insurance Brokers' and CoverName = 'Car Hire - 30 Days'
 
--------
 update RiskItems
 set CoverName = 'Excess waiver - R 2 000'
 where Product = 'Product7 Insurance Brokers'	and CoverName = 'Excess Waiver Basic'
 
--------
 update RiskItems
 set CoverName = 'Excess Waiver - R6500'
 where Product = 'Product8' and CoverName = 'Excess Waiver Basic'
 
--------
 update RiskItems
 set CoverName = 'OneVAP Excess waiver'
 where Product = 'Product1'	and CoverName = 'Excess Waiver Basic'
 
--------
 update RiskItems
 set CoverName = 'Product13 Excess waiver'
 where Product = 'Product13 Insurance Brokers'	and CoverName = 'Excess Waiver Basic'
 
---------
 update RiskItems
 set CoverName = 'Car Hire - Similar Vehicle 30 Days'
 where RiskID In (N'50936ED1-CF03-4944-ACD3-FAD1D90067F9', N'A30E1E54-5E5A-4ABB-ACF2-3D761585758E')
 
---------
 update RiskItems
 set CoverName = 'Excess Waiver'
 where RiskID In (N'640F43CD-1697-48DC-9EFF-9858F50531EC', N'5FD74162-E657-481E-A88E-8573CF7E8B53', N'C5EA7398-2EB1-413F-8F90-819EDD351CAF', N'02A1DB02-02D8-4EC4-BD57-DC0C9CFD2EC4', N'7476E686-B361-424F-A4EF-8FEB01468DF7', N'EACD19E9-B7EB-463F-A824-A30B96805877', N'67F73E2F-E253-46BA-B0CF-4EC233206B50', N'77529A1E-8A40-439F-8579-0D3CD6B1038F', N'7500EC80-95A5-4036-AFDB-580DAC9AD251', N'74853B85-83A5-43CB-9E20-45F3689F1F94', N'A47AD66C-460E-425B-9850-42120BE85C92', N'E01B338A-6288-4783-9CAB-377E457238FC', N'4C371D9C-864A-48A9-BC66-50BCC99B5B7D', N'B6298B2D-8EC5-414B-B2EE-65B0DA7C4EBA', N'AF40D633-BD48-453C-8846-16C0DF8D4D6E', N'E706A24C-DAD3-450E-8546-1328EA38AB9D', N'4FAC6329-C970-47F9-966E-6EC20C465EFD', N'E86B6CD8-F979-48C6-8E9E-9210D73BE29C', N'BC56047E-7AA3-4986-84B3-91823BD6531D', N'ACD2C5FA-0226-4164-8F87-C51A0EF7A4E1', N'99D2ABA0-44D8-4EDD-9A6E-B16983D8AD5A', N'43944887-E64C-4B2B-B184-FA79D8A9A420', N'6F64471E-C14B-4F9F-A260-FE161C8F1F44', N'9237C1CC-1B9B-49F5-96A4-E300DEAEB522', N'02A3B8D2-EA0A-4F97-B63B-DA07BC1157D8', N'9A9DD659-E6F1-42A5-AC90-33879234ACCD', N'3E2874FD-D990-4ADD-9022-00DE720AE019', N'BF99923B-4FEA-4CDE-A0FF-4B3FD77DC8EC', N'16CE7173-F03D-415D-AC19-438FCED13D97', N'A7FCD110-F134-4A8B-BF13-D06B04D7AD1A', N'48502ED4-9DB3-43FF-A1D1-B197B026FA6F', N'0BE1D08A-2E48-495F-A2DA-A944D491A07E', N'F88EAEB5-1650-4B67-B45A-6737AB9085AF', N'521B107B-8498-4B73-90D4-9F6100ABB3FA', N'09BE7176-5B4D-4145-AEB6-AED2C08F38A3', N'A5731ED2-D048-4D5F-8F7D-B101AC8A001A', N'A6D570F7-B187-4F2C-82EB-CF2E94C28835', N'EBB1CB56-4D30-4BCB-B953-95ABA60F29ED', N'620F1CBA-ED29-4418-8712-D9098AB08E58', N'0E7F582E-EB37-4DF7-A1E1-6A71A5631618', N'F9E5EC09-23AD-4D25-999B-098B8226B4DF', N'41569A6E-B7BC-40BE-8961-3ED02D135C32', N'9CDDB911-CF56-41F1-BC5B-EC008709AABF', N'DBE53385-FE73-401D-8EA8-FFDCE456FEA2', N'70016531-4207-442D-9E58-BFDF8EDE36E9', N'72668B94-87C3-443A-8F48-3DECC053A39C')
